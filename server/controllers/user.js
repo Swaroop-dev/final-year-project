9306ce20551d17b920cf27851b5947da21040ea6
swaroop <swaroopaee98@gmail.com>
2021-04-23 23:03:22 +0530

    user controllers and user routes added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,20 @@
 const User = require("../models/user")
 
 
+exports.getUserById = (req, res, id, next) => {
+    User.findById(id).exec((err, user) => {
+        if (err) {
+            res.status(403).json({
+                error:'could not find the user with the given id'
+            })
+        }
+        req.profile = user;
+        next();
+    })
+}
+
+exports.addCsv = (req, res) => {
+    
+}
+
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const {isAdmin,isSignedIn,isAuthenticated }=require('../controllers/auth')
+const {getUserById,addCsv }=require('../controllers/user')
 
 
+router.param('userId',getUserById)
+
+router.post('/addCsv/users/:userId',isSignedIn,isAuthenticated,isAdmin,addCsv)
