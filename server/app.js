cde3151d060acc401edae7164c9319cbe9358e8c
swaroop <swaroopaee98@gmail.com>
2021-04-23 20:30:58 +0530

    server is setup and database is connected

--- server/app.js
+++ server/app.js
@@ -1,2 +1,39 @@
-const express = require('express')
-const app = express()
+//required dependicies
+const mongoose=require("mongoose")
+const express=require("express")
+const bodyParser=require("body-parser")
+const  cors=require("cors")
+const cookieParser=require("cookie-parser")
+require('dotenv').config()
+
+
+
+//server and database
+
+const app=express()
+const port=process.env.PORT||8000
+
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("..IS CONNECTING");
+  });
+
+
+ app.use(bodyParser.json());
+ app.use(cors());
+app.use(cookieParser());
+ 
+
+//server running
+app.listen(port, () => {
+  console.log(`app is running at port number ${port}`);
+});
